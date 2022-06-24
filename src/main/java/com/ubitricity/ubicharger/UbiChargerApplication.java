a8ec67598692773577992eb97020a8d8ffd5e767
AymenBelHabib <aymen.bl.habib@gmail.com>
2022-06-24 11:19:08 +0200

    first push

---
+++ src/main/java/com/ubitricity/ubicharger/UbiChargerApplication.java
@@ -0,0 +1,13 @@
+package com.ubitricity.ubicharger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UbiChargerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UbiChargerApplication.class, args);
+	}
+
+}
