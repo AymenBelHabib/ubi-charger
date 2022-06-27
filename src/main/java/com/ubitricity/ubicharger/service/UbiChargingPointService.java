a55dfebfec8e65064a7a91a431691aab0e936dca
AymenBelHabib <aymen.bl.habib@gmail.com>
2022-06-27 05:26:40 +0200

    added service interfaces

---
+++ src/main/java/com/ubitricity/ubicharger/service/UbiChargingPointService.java
@@ -0,0 +1,6 @@
+package com.ubitricity.ubicharger.service;
+
+public interface UbiChargingPointService {
+    int addCar(Integer cpNumber);
+    int removeCar(Integer cpNumber);
+}
---
+++ src/main/java/com/ubitricity/ubicharger/service/UbiStationService.java
@@ -0,0 +1,9 @@
+package com.ubitricity.ubicharger.service;
+
+import com.ubitricity.ubicharger.domain.entity.UbiStation;
+import com.ubitricity.ubicharger.service.dto.UbiStationDTO;
+
+public interface UbiStationService {
+
+    UbiStationDTO getUbiStationReport ();
+}
